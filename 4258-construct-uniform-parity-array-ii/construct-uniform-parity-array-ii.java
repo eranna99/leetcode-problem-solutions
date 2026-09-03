3a5d4c08de63b6452c3186cb93bce5fea29766d0
eranna99 <150374771+eranna99@users.noreply.github.com>
2026-09-03 15:47:14 +0530

    Clean up commented code in uniformArray method
    Removed commented-out code for clarity and focus.

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -1,20 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n = nums1.length;
-        // int nums2[] = new int[2];
-        // int ev=0;
-        // int od=0;
-        // for(int i = 0; i < n ; i++ ){
-        //      if(nums1[i]%2==0){
-        //         ev++;
-        //      }  
-        //      else{
-        //         od++;
-        //      }
-
-        // }
-        // if((ev >0 )&& od>0) return false;
-        // else return true;
 
         int minOdd = Integer.MAX_VALUE;
         int minEven = Integer.MAX_VALUE;
